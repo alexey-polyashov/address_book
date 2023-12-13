64649d417111e7b5bc912da094ef8f7768ad3ac9
alexey-polyashov <polyashofff@yandex.ru>
2023-12-13 23:17:51 +0300

    init commit

---
+++ src/test/java/ru/mikron/address_book/AddressBookApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mikron.address_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
